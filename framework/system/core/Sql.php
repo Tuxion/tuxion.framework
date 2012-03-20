134db96742db2af041650d3002325cedc2dd60c8
Avaq <aldwin.vlasblom@gmail.com>
2012-03-20 22:59:52 +0100

    Started creating the foundations of Sql.php.

--- framework/system/core/Sql.php
+++ framework/system/core/Sql.php
@@ -3,6 +3,22 @@
 class Sql
 {
   
+  private $connections = [];
   
+  public function __destruct(){
+    
+  }
+  
+  public function init(){
+    
+  }
+  
+  public function query(){
+    
+  }
+  
+  public function queries(){
+    
+  }
   
 }
